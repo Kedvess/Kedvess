8b99330029eabe97418600bb3d1eb7e19b05d94b
Kedvess <95028329+Kedvess@users.noreply.github.com>
2025-11-02 21:17:27 +0200

    Delete pages/API/grid.js

--- pages/API/grid.js
+++
@@ -1,9 +0,0 @@
-// pages/api/grid.js
-
-export default function handler(req, res) {
-  res.status(200).json({
-    version: "CRYSTAL_V9.8",
-    Δstatus: "LIVE",
-    message: "Node grid connected successfully 🔮"
-  });
-}
